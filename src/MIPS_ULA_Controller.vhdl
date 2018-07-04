729f9f174311bf81ea09c6deae1e24a62e655d2c
MatheusSiade <matheussiadef@outlook.com>
2018-07-04 19:59:45 -0300

    Feitas as operações do ULA control tipo R

--- src/MIPS_ULA_Controller.vhdl
+++ src/MIPS_ULA_Controller.vhdl
@@ -15,14 +15,17 @@ end MIPS_ULA_Controller;
 -- 0001 -> or
 -- 0010 -> soma
 -- 0011 -> somau
+
 -- 0100 -> sub
 -- 0101 -> subu
 -- 0110 -> slt
 -- 0111 -> sltu
+
 -- 1000 -> nor
 -- 1001 -> xor
 -- 1010 -> sll
 -- 1011 -> srl
+
 -- 1100 -> sra
 -- 1101 -> rtr
 -- 1110 -> rtl
@@ -34,7 +37,41 @@ architecture behavioral of MIPS_ULA_Controller is
 	    case ALUop is
             when "100" =>
                 case intFunct is
+                    when "101000"=> -- AND
+                        ALU <= "0000";
+                    when "100101"=> -- OR
+                        ALU <= "0001";
+                    when "100000"=> -- ADD
+                        ALU <= "0010";
+                    when "100001"=> -- ADDU
+                        ALU <= "0011";
+
+                    when "100010" => --SUB
+                        ALU <= "0100";
+                    when "100011" => --SUBU
+                        ALU <= "0101";
+                    when "101010" => --SLT
+                        ALU <= "0110";
+                    when "101011" => --SLTU
+                        ALU <= "0111";
                     
+                    when "100111" => --NOR
+                        ALU <= "1000";
+                    when "100110" => --xor
+                        ALU <= "1001";
+                    when "000000" => --sll
+                        ALU <= "1010";
+                    when "000010" => --srl
+                        ALU <= "1011";
+                    
+                    when "000011" => --sra
+                        ALU <= "1100";
+                    when "000010" => --rtr
+                        ALU <= "1101";
+
+                    when others => 
+                        ALU <= "1111";
+                    end case;
         end case;
     end process;
 
