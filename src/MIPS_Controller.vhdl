c53d89cd3a6ed99eb30b93105c64c6f3e31a6994
eduardocserra <eduardocserra@outlook.com>
2018-07-04 18:03:06 -0300

    Correção da entidade do controle do MIPS

--- src/MIPS_Controller.vhdl
+++ src/MIPS_Controller.vhdl
@@ -3,8 +3,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity MIPS_Controller is
-
-	generic(WSIZE : natural := 32);
 	
 	port(intruction_opcode : in std_logic_vector(5 downto 0);
         regDST, jump, branch, memRead, memToReg, ALUop, memWrite, ALUsrc, regWrite : out std_logic;
