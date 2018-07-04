4bd43f31ba0b66ba59e8494356ae1ad45bdbab61
André Laranjeira <AndreCaldasLaranjeira@gmail.com>
2018-07-04 15:22:00 -0300

    Instanciated muxes. Added TODO list, comments and generics.

--- src/MIPS_Processor_Unicycle.vhdl
+++ src/MIPS_Processor_Unicycle.vhdl
@@ -4,11 +4,10 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity MIPS_Processor_Unicycle is
 
-	-- TODO: Code the ports and generics of this entity.
-
-	generic(MIF_FILE_INSTRUCTION : string := "RAM.mif";
-			  MIF_FILE_DATA : string := "DATA.mif";
-			  inst_types_bits : natural := 1;
+	generic(MIF_FILE_DATA : string := "DATA.mif";
+			  MIF_FILE_INSTRUCTION : string := "RAM.mif";
+			  BREG_SIZE : natural := 5;
+			  TYPES_SIZE : natural := 1;
 			  WSIZE : natural := 32);
 	
 	port(clock : in STD_LOGIC;
@@ -17,6 +16,8 @@ entity MIPS_Processor_Unicycle is
 end MIPS_Processor_Unicycle;
 
 architecture behavioral of MIPS_Processor_Unicycle is
+
+-- Components
 
 	component MIPS_BREG is
 
@@ -29,11 +30,6 @@ architecture behavioral of MIPS_Processor_Unicycle is
 			  Reg1, Reg2 : out std_logic_vector(WSIZE-1 downto 0));
 		  
 	end component;
-	
-	-- Note: This is an instruction memory, not a general memory.
-	
-	-- TODO: When convinient, this component should be renamed to better reflect
-	-- it's nature and restructured to allow for better modularity.
 	
 	component MIPS_Memory is
 
@@ -85,18 +81,49 @@ architecture behavioral of MIPS_Processor_Unicycle is
 		  
 	end component;
 
-signal branch, read_DATA_MEM, write_BREG, write_DATA_MEM : STD_LOGIC;
-signal mux_BREG_WD, mux_BREG_WR, mux_ULA_opB : STD_LOGIC;
-signal opcode_type : STD_LOGIC_VECTOR(inst_types_bits downto 0);
+-- Control signals
 	
-signal BREG_R1, BREG_R2, BREG_WR : STD_LOGIC_VECTOR(WSIZE-1 downto 0);
+signal branch, read_DATA_MEM, write_BREG, write_DATA_MEM : STD_LOGIC;
+signal sel_BREG_WD, sel_BREG_WR, sel_ULA_opB : STD_LOGIC;
+signal opcode_type : STD_LOGIC_VECTOR(TYPES_SIZE downto 0);
+
+-- Data signals
+	
+signal BREG_R1, BREG_R2, BREG_WR : STD_LOGIC_VECTOR(BREG_SIZE-1 downto 0);
 signal BREG_D1, BREG_D2, BREG_WD : STD_LOGIC_VECTOR(WSIZE-1 downto 0);
 signal DATA_MEM_output : STD_LOGIC_VECTOR(WSIZE-1 downto 0);
+signal instruction : STD_LOGIC_VECTOR(WSIZE-1 downto 0); 
 signal PC_plus_4, next_PC, sxt_imm : STD_LOGIC_VECTOR(WSIZE-1 downto 0);
 signal ULA_opA, ULA_opB, ULA_result : STD_LOGIC_VECTOR(WSIZE-1 downto 0);
 	
 begin
 
+-- Multiplexers:
 
-	
+	Mux_BREG_WD : Multiplexer2to1
+		generic map(WSIZE => WSIZE)
+		port map(input1 => ULA_result, 
+					input2 => DATA_MEM_output,
+					selector => sel_BREG_WD,
+					output => BREG_WD);
+
+	Mux_BREG_WR : Multiplexer2to1
+		generic map(WSIZE => BREG_SIZE)
+		port map(input1 => instruction(20 downto 16), 
+					input2 => instruction(15 downto 11),
+					selector => sel_BREG_WR,
+					output => BREG_WR);
+				
+	Mux_ULA_opB : Multiplexer2to1
+		generic map(WSIZE => WSIZE)
+		port map(input1 => BREG_D2, 
+					input2 => sxt_imm,
+					selector => sel_ULA_opB,
+					output => ULA_opB);		
+				
 end behavioral;
+
+-- TODO list:
+--		Finish behavioral architecture of MIPS_Processor_Unicycle.
+--		Break down MIPS_Memory component. 
+--		Add generic for number of registers in MIPS_BREG component. 
