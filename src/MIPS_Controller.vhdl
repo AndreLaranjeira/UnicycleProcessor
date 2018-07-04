92738c4c3bc5d96ed30bc0107d01ae76fdf84733
eduardocserra <eduardocserra@outlook.com>
2018-07-04 18:03:06 -0300

    Criação do case para as saídas do controle

--- src/MIPS_Controller.vhdl
+++ src/MIPS_Controller.vhdl
@@ -3,15 +3,22 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity MIPS_Controller is
-
-	generic(WSIZE : natural := 32);
 	
-	port(intruction_opcode : in std_logic_vector(5 downto 0);
+	port(int_opcode : in std_logic_vector(5 downto 0);
         regDST, jump, branch, memRead, memToReg, ALUop, memWrite, ALUsrc, regWrite : out std_logic;
 		  
 		  
 end MIPS_Controller;
 
 architecture behavioral of MIPS_Controller is
+begin
+    Operation: process(int_opcode)
+	    begin
+	    case int_opcode is
+			when "000000" => 
+            
+        end case;
+		
+	end process;
 
 end behavioral;
