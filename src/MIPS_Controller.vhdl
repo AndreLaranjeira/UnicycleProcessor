3d3212c037a378a0a19aecdf605e4139c5d944d2
MatheusSiade <matheussiadef@outlook.com>
2018-07-04 18:35:14 -0300

    Case "000000" implementado

--- src/MIPS_Controller.vhdl
+++ src/MIPS_Controller.vhdl
@@ -5,8 +5,8 @@ use IEEE.NUMERIC_STD.ALL;
 entity MIPS_Controller is
 	
 	port(int_opcode : in std_logic_vector(5 downto 0);
-        regDST, jump, branch, memRead, memToReg, ALUop, memWrite, ALUsrc, regWrite : out std_logic;
-		  
+        regDST, jump, branch, memRead, memToReg, memWrite, ALUsrc, regWrite : out std_logic;
+	ALUop out std_logic_vector (1 downto 0));
 		  
 end MIPS_Controller;
 
@@ -15,8 +15,16 @@ begin
     Operation: process(int_opcode)
 	    begin
 	    case int_opcode is
-			when "000000" => 
-            
+			when "000000" =>
+				regDST <= '1';
+				jump <= '0';
+				branch <= '0';
+				memRead <= '0';
+				memToReg <= '0';
+				memWrite <= '0';
+				ALUsrc <= '0';
+				regWrite <= '1';
+				ALUop <= '10';
         end case;
 		
 	end process;
